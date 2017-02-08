d657b4d3f9e625def774a3edfc5963d9ca82d704
ZhaoC <edwardcai1993@gmail.com>
2017-02-07 19:53:03 -0500

    update with comments

--- Python basic practice/p_json.py
+++ Python basic practice/p_json.py
@@ -2,7 +2,9 @@
 import json
 
 json_data = json.loads(open("./test_files/samplejson.json").read())
-print json_data["code"][0]
+
+#print the list items in the json data
+print json_data["code"][0] 
 print json_data["code"][1]
 print json_data["code"][2]
 print json_data["code"][3]
